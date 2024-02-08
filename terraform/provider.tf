beeb3eb610c823ec5023fd0a80b7c88e1002c257
noko-git <kotaroyamano0725@gmail.com>
2024-02-09 00:30:18 +0900

    fix workbench tf #2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.15.0"
+    }
+  }
+}
+
 provider "google" {
   project = var.gcp_project
   region  = var.gcp_region
