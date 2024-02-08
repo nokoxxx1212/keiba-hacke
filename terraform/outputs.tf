1e21df282f1de3361eca18b25523bf0e4c682886
noko-git <kotaroyamano0725@gmail.com>
2024-02-08 22:57:06 +0900

    fix workbench tf #2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,22 +10,47 @@ output "bigquery_dataset_self_link" {
 }
 
 # Notebooks instance outputs
+#output "instance_name" {
+#  description = "The name of the Vertex AI Workbench instance."
+#  value       = google_notebooks_instance.instance.name
+#}
+#
+#output "instance_location" {
+#  description = "The location of the Vertex AI Workbench instance."
+#  value       = google_notebooks_instance.instance.location
+#}
+#
+#output "instance_machine_type" {
+#  description = "The machine type of the Vertex AI Workbench instance."
+#  value       = google_notebooks_instance.instance.machine_type
+#}
+#
+#output "instance_state" {
+#  description = "The state of the Vertex AI Workbench instance."
+#  value       = google_notebooks_instance.instance.state
+#}
+
 output "instance_name" {
   description = "The name of the Vertex AI Workbench instance."
-  value       = google_notebooks_instance.instance.name
-}
+  value       = google_workbench_instance.instance.name
+} 
 
 output "instance_location" {
   description = "The location of the Vertex AI Workbench instance."
-  value       = google_notebooks_instance.instance.location
-}
+  value       = google_workbench_instance.instance.location
+} 
 
 output "instance_machine_type" {
   description = "The machine type of the Vertex AI Workbench instance."
-  value       = google_notebooks_instance.instance.machine_type
-}
+  value       = google_workbench_instance.instance.gce_setup.machine_type
+} 
 
 output "instance_state" {
   description = "The state of the Vertex AI Workbench instance."
-  value       = google_notebooks_instance.instance.state
+  value       = google_workbench_instance.instance.state
+} 
+
+output "instance_self_link" {
+  description = "The URI of the created resource."
+  value       = google_workbench_instance.instance.self_link
 }
